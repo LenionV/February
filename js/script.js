baa20c17d101d85387f9fbd7f44f1dbae9a66796
LenionV <71942398+LenionV@users.noreply.github.com>
2021-02-08 01:02:11 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,47 +1,50 @@
-	Data = new Date();
-	Year = Data.getFullYear();
-	Month = Data.getMonth();
-	Day = Data.getDate();
-	Hour = Data.getHours();
-	Minutes = Data.getMinutes();
-	Seconds = Data.getSeconds();
-	var praz1=8;
-	var praz2=14;
+Data = new Date();
+Year = Data.getFullYear();
+Month = Data.getMonth();
+Day = Data.getDate();
+Hour = Data.getHours();
+Minutes = Data.getMinutes();
+Seconds = Data.getSeconds();
+var praz1=8;
+var praz2=14;
+
+
+function text_out(){
+	var p;
+	var aaa=0;
+	p = document.getElementById('text_change');
+	p.innerHTML =Day;
 	
-	
-	function text_out(){
-		var p;
-		p = document.getElementById('text_change');
-		p.innerHTML =Day;
-		
-		if (Day==praz1)
-		{
-			document.getElementById("text_change").style.color = "pink";
-			
-		}
-		else 
-		{
-			document.getElementById("text_change").style.color = "red";
-		}
-		
-		if (Day==praz2)
-		{
-			document.getElementById("text_change").style.color = "pink";
-			
-		}
-		else 
-		{
-			document.getElementById("text_change").style.color = "red";
-		}
-		
+	if (Day==praz1)
+	{
+		document.getElementById("text_change").style.color = "pink";
+		aaa=1;
+	}
+	else 
+	{
+		document.getElementById("text_change").style.color = "red";
+	}
+	if (aaa!=1)
+	if (Day==praz2)
+	{
+		document.getElementById("text_change").style.color = "pink";
 		
 	}
-	
-	function start(){
-		if (Day==praz1){
-			window.open("./1/1.html");
-		}
-				if (Day==praz2){
-			window.open("./2/1.html");
-		}
+	else 
+	{
+		document.getElementById("text_change").style.color = "red";
 	}
+	
+	
+}
+
+function start(){
+	if (Day==praz1){
+		window.open("./1/1.html");
+	}
+	if (Day==praz2){
+		window.open("./2/1.html");
+	}
+	}	function menu(){
+	window.open("menu.html");
+}
